2655c33eb0e7c70c2985345411b2dd0a58eb9a7a
bdavis93 <s.blakedavis1@gmail.com>
2022-06-08 15:44:08 -0500

    add iteration function to load data for each post

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './Components/header';
 import { Subreddit } from './Components/subreddit';
 import { Reddit } from './Components/reddit';
 
+
 function App() {
+
+    const [state, setState] = useState({name: 'Blake', title: 'my post', selftext: 'sample text'},
+    {name: 'Blake', title: 'my post', selftext: 'sample text' },
+    {name: 'Blake', title: 'my post', selftext: 'sample text' });
+
+    function renderPosts() {
+      const userPosts = [];
+      for(let i = 0; i < state.length; i++) {
+        let name = state[i].name;
+        let title = state[i].title;
+        let text = state[i].selftext;
+        userPosts.push(<Reddit text={text}/>);
+      }
+      return userPosts;
+    }
+    
   return (
     <div>
       <Header />
       <Subreddit />
-      <Reddit />
+      {renderPosts()}
     </div>
   );
 }
--- src/Components/reddit.js
+++ src/Components/reddit.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import redditDog from '../reddit-dog-pic.jpeg';
 
-export const Reddit = () => {
+export const Reddit = ({ text }) => {
+
+    
     return (
         <div class="redditContainer">
-            <div class="reddit">
-                <h2>Text from Reddit title......</h2>
-                <img src={redditDog}></img>
-                <p>Comments</p>
-            </div>
-             <div class="reddit">
-                <h2>Text from Reddit title......</h2>
-                <img src={redditDog}></img>
-                <p>Comments</p>
-            </div>
+                    <div class="reddit">
+                        <h2>{text}</h2>
+                        <img src={redditDog}></img>
+                        <p>Comments</p>
+                    </div>
+                    
+            
         </div>
     )
 }
